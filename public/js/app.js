8b5048f93ee0f800d1021f6f45cef7a201ad41c1
Student <student@PNCLB13-10.pnc.lan>
2021-05-22 09:10:30 +0700

    FROME PHEARAK_THREE

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,22 @@
-// ======================= |Close Eye Password| ======================= //
+// ======================= |Search| ======================= //
 
-const searchBar = document.querySelector("#search");
-const btnSearch = document.querySelector("button");
+// const searchBar = document.querySelector("#search");
+// const btnSearch = document.querySelector("button");
 
-btnSearch.onclick = () => {
-    searchBar.classList.add("active");
-    btnSearch.focus();
-}
-let messages = []
+// // btnSearch.onclick = () => {
+// //     searchBar.classList.add("active");
+// //     btnSearch.focus();
+// // }
+// let messages = []
 
-// ======================= |Close Eye Password| ======================= //
+// ======================= |Search| ======================= //
 
 // ======================= |Send Message| ======================= //
 
-function getMessage(e) {
-    e.preventDefault();
+let Wrapper = document.querySelector(".content_Chat");
+const Message = document.querySelector("#messages");
 
-    let Wrapper = document.querySelector(".content_Chat");
+function getMessage(e) {
     
     let Chat_Box = document.createElement("div");
     Chat_Box.className = "chat-box";
@@ -37,40 +37,34 @@ function getMessage(e) {
     images.src = "../image/PHEARA_ENG.jpg";
     chat_outgoing.appendChild(images);
     
-    const Message = document.querySelector("#messages");
     P.textContent += Message.value;
 }
 
 const btnSend = document.querySelector("#paper_plane");
 btnSend.addEventListener("click", getMessage);
-
 // ======================= |Send Message| ======================= //
 
-let form = document.createElement("form");
-form.className = "typing-area";
+// ======================= |USER-LIST| ======================= //
 
-let button = document.createElement("button");
+// let users_list = document.createElement("div");
+// users_list.className = "users-list";
+
+let a_href = document.createElement("href");
+console.log(a_href);
+
+// ======================= |USER-LIST| ======================= //
+
+// ================================| Key Up |=============================== //
+Message.addEventListener("keyup", function (event){
+    if(event.keyCode === 13){
+        getMessage();
+    }
+})
+// ================================| Key Up |=============================== //
 
 // ================================| Emoji |=============================== //
-
 const Emoji = document.querySelector("#emoji");
-
-// import { EmojiButton } from '@joeattardi/emoji-button';
-
-// const picker = new EmojiButton();
-
-// picker.on('emoji', selection => {
-    
-// });
-
-// Emoji.addEventListener('click', () => picker.togglePicker(Emoji));
-
-// ================================| Emoji |=============================== //
-
-
 const picker = new EmojiButton();
-
-// Emoji selection
 document.addEventListener('DOMContentLoaded', () =>{
     picker.on('emoji', emoji =>{
         document.querySelector('input').value += emoji;
@@ -79,3 +73,34 @@ document.addEventListener('DOMContentLoaded', () =>{
         picker.togglePicker(Emoji);
     });
 });
+// ================================| Emoji |=============================== //
+
+// ================================| Bold Character |=============================== //
+const btnBold = document.querySelector("#bold");
+btnBold.onclick = () => {
+    if (Message.type == "text") {
+        Message.style.fontWeight = "bold";
+    }else {
+        Message.style.fontWeight = "text";
+    }
+}
+// ================================| Bold Character |=============================== //
+
+// ================================| Italic Character |=============================== //
+const btnItalic = document.querySelector("#italic");
+btnItalic.onclick = () => {
+    if (Message.type == 'text') {
+        Message.style.fontStyle = 'italics';
+        btnItalic.classList.add("active");
+    }else {
+        getPass.type = "text";
+        Message.style = "text";
+    }
+}
+// ================================| Italic Character |=============================== //
+
+// ================================| Voice Sound |=============================== //
+
+
+
+// ================================| Voice Sound |=============================== //
